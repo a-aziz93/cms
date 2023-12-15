2a8873c2a328ddac8e38ded9877a4f2478fea670
ubuntu <a.atoev93@gmail.com>
2023-12-15 20:41:05 +0500

    Commit

--- composeApp/src/commonMain/kotlin/ui/signin/SignInUi.kt
+++ composeApp/src/commonMain/kotlin/ui/signin/SignInUi.kt
@@ -1,15 +1,23 @@
 package ui.signin
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.TextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import coroutineDispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun SignInUi(component: SignInComponent) {
@@ -18,23 +26,54 @@ fun SignInUi(component: SignInComponent) {
     val password = component.password.collectAsState(coroutineDispatchers.main)
     val inProgress = component.inProgress.collectAsState()
 
-    Column {
-        TextField(
-            value = login.value, 
-            onValueChange = component::onLoginChanged
-        )
+    val scope = rememberCoroutineScope()
 
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize().padding(horizontal = 24.dp)
+    ){
         TextField(
+            label = {
+                Text("Enter your Email")
+                    },
+            value = login.value,
+            onValueChange = component::onLoginChanged,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                autoCorrect = false,
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            label = {
+                Text("Enter your Password")
+                    },
             value = password.value,
             onValueChange = component::onPasswordChanged,
+            // Enable user input from the keyboard
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                autoCorrect = false,
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Next
+            ),
+            // Enable password input type by showing dots for password security
             visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
         )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            scope.launch {
 
-        if (inProgress.value) {
-            CircularProgressIndicator()
-        } else {
-            Button(onClick = component::onSignInClick){}
+            }
+        }){
+            Text("Login")
         }
     }
 }
