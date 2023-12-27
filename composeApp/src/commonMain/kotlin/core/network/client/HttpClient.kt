22c01e8461c2f0538c5848948e7984b7965bd644
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2023-12-27 23:52:04 +0500

    Commit

--- composeApp/src/commonMain/kotlin/core/network/client/HttpClient.kt
+++ composeApp/src/commonMain/kotlin/core/network/client/HttpClient.kt
@@ -1,4 +1,4 @@
-package core.network
+package core.network.client
 
 import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
@@ -6,17 +6,24 @@ import io.ktor.client.plugins.logging.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 import org.koin.core.annotation.Single
+import de.jensklingenberg.ktorfit.Ktorfit
+import de.jensklingenberg.ktorfit.create
 
 @Single
-fun PlatformHttpClient(): HttpClient = createPlatformHttpClient {
-    install(ContentNegotiation) {
-        json(Json {
-            ignoreUnknownKeys = true
-        })
-    }
-
-    install(Logging) {
-        logger = Logger.DEFAULT
-        level = LogLevel.ALL
-    }
-}
+fun createHttpClient(
+    httpClientEngine: HttpClientEngine,
+        json: Json,
+            enableNetworkLogs: Boolean
+            ): Ktorfit {
+                val client = HttpClient(httpClientEngine) {
+                        defaultRequest { url("https://pokeapi.co/api/v2/") }
+                                install(ContentNegotiation) { json(json) }
+                                        if (enableNetworkLogs) {
+                                                    install(Logging) {
+                                                                    logger = Logger.DEFAULT
+                                                                                    level = LogLevel.INFO
+                                                                                                }
+                                                                                                        }
+                                                                                                            }
+                                                                                                                return Ktorfit.Builder().httpClient(client).build()
+                                                                                                                }
