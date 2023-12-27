276e10ee3b15fc366581fa6262d89c9cb2755109
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2023-12-27 23:43:52 +0500

    Delete composeApp/src/wasmJsMain/kotlin/core/network/JsHttpClientFactory.kt

--- composeApp/src/wasmJsMain/kotlin/core/network/JsHttpClientFactory.kt
+++
@@ -1,8 +0,0 @@
-package core.network
-
-import io.ktor.client.*
-import io.ktor.client.engine.js.*
-
-actual fun createPlatformHttpClient(config: HttpClientConfig<*>.() -> Unit = {}): HttpClient {
-    return HttpClient(Js,config)
-}
