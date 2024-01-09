1a2a9b4ee4293ef7b841459eefb9b2f983a28983
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2024-01-09 13:15:55 +0500

    Update and rename Page.kt to PageRequest.kt

--- shared/src/commonMain/kotlin/core/crud/model/request/PageRequest.kt
+++ shared/src/commonMain/kotlin/core/crud/model/request/PageRequest.kt
@@ -3,7 +3,7 @@ package core.crud.model.request
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Page(
+data class PageRequest(
     val index: Int = 1,
     val size: Int = 1,
 )
