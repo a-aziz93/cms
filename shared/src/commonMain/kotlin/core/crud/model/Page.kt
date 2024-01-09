2816b5e54e4c33575261ff501d51e79a354dc534
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2024-01-09 13:29:00 +0500

    Update Page.kt

--- shared/src/commonMain/kotlin/core/crud/model/Page.kt
+++ shared/src/commonMain/kotlin/core/crud/model/Page.kt
@@ -3,7 +3,7 @@ package core.crud.model.request
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class PageReques(
+data class Page(
     val index: Int = 1,
     val size: Int = 1,
 )
