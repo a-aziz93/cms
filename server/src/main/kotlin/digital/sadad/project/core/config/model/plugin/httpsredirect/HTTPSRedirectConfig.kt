3f41fc8de5b239875b923f5dd1040344656736e2
Atoev Aziz <a.atoev93@gmail.com>
2024-01-24 20:48:52 +0500

    Commit

--- server/src/main/kotlin/digital/sadad/project/core/config/model/plugin/httpsredirect/HTTPSRedirectConfig.kt
+++ server/src/main/kotlin/digital/sadad/project/core/config/model/plugin/httpsredirect/HTTPSRedirectConfig.kt
@@ -1,9 +1,6 @@
 package digital.sadad.project.core.config.model.plugin.httpsredirect
 
 import digital.sadad.project.core.config.model.plugin.PluginConfig
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.*
 
 class HTTPSRedirectConfig(
     enable: Boolean? = null,
