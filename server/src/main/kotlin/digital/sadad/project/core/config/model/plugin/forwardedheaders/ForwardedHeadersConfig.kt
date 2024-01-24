2c3e80679cebb3416837430f656b106b08c85345
Atoev Aziz <a.atoev93@gmail.com>
2024-01-24 20:30:50 +0500

    Commit

--- server/src/main/kotlin/digital/sadad/project/core/config/model/plugin/forwardedheaders/ForwardedHeadersConfig.kt
+++ server/src/main/kotlin/digital/sadad/project/core/config/model/plugin/forwardedheaders/ForwardedHeadersConfig.kt
@@ -1,7 +1,17 @@
 package digital.sadad.project.core.config.model.plugin.forwardedheaders
 
 import digital.sadad.project.core.config.model.plugin.PluginConfig
+import io.ktor.http.*
 
 class ForwardedHeadersConfig(
     enable: Boolean? = null,
+    val useFirst: Boolean? = null,
+    val useLast: Boolean? = null,
+    val skipLastProxies: Int? = null,
+    val skipKnownProxies: List<String>? = null,
+    val xForwardedHostHeaders: List<String>? = null,
+    val xForwardedProtoHeaders: List<String>? = null,
+    val xForwardedForHeaders: List<String>? = null,
+    val xForwardedHttpsFlagHeaders: List<String>? = null,
+    val xForwardedPortHeaders: List<String>? = null,
 ) : PluginConfig(enable)
--- server/src/main/kotlin/digital/sadad/project/core/plugin/forwardedheaders/ForwardedHeaders.kt
+++ server/src/main/kotlin/digital/sadad/project/core/plugin/forwardedheaders/ForwardedHeaders.kt
@@ -6,8 +6,51 @@ import io.ktor.server.plugins.forwardedheaders.*
 
 fun Application.configureForwardedHeaders(config: ForwardedHeadersConfig) {
     if (config.enable == true) {
-        install(ForwardedHeaders) // WARNING: for security, do not include this if not behind a reverse proxy
-        install(XForwardedHeaders) // WARNING: for security, do not include this if not behind a reverse proxy {
+        // WARNING: for security, do not include this if not behind a reverse proxy
+        install(ForwardedHeaders) {
+            config.useFirst?.let {
+                if (it) {
+                    useFirstValue()
+                }
+            }
+
+            config.useLast?.let {
+                if (it) {
+                    useLastValue()
+                }
+            }
+
+            config.skipLastProxies?.let { skipLastProxies(it) }
+
+            config.skipKnownProxies?.let { skipKnownProxies(it) }
+        }
+
+        // WARNING: for security, do not include this if not behind a reverse proxy
+        install(XForwardedHeaders) {
+
+            config.xForwardedHostHeaders?.let { hostHeaders + it }
+            config.xForwardedProtoHeaders?.let { protoHeaders + it }
+            config.xForwardedForHeaders?.let { forHeaders + it }
+            config.xForwardedHttpsFlagHeaders?.let { httpsFlagHeaders + it }
+            config.xForwardedPortHeaders?.let { portHeaders + it }
+
+            config.useFirst?.let {
+                if (it) {
+                    useFirstProxy()
+                }
+            }
+
+            config.useFirst?.let {
+                if (it) {
+                    useLastProxy()
+                }
+            }
+
+            config.skipLastProxies?.let { skipLastProxies(it) }
+
+            config.skipKnownProxies?.let { skipKnownProxies(it) }
+
+        }
 
     }
 }
